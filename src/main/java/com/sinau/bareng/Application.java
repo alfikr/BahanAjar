6294c3da656cac2bebb5fa62777550d7180961ce
danang <daffodil1411@gmail.com>
2022-04-11 23:14:06 +0700

    add singleton

--- src/main/java/com/sinau/bareng/Application.java
+++ src/main/java/com/sinau/bareng/Application.java
@@ -1,7 +1,38 @@
 package com.sinau.bareng;
 
+import com.sinau.bareng.singleton.BelajarSingleton;
+
+import java.util.Scanner;
+
+import static java.lang.Thread.sleep;
+
 public class Application {
     public static void main(String[] args) {
+        Thread b = new Thread(()-> {
+            while (true){
+                if (BelajarSingleton.getInstance().getVal()!=null){
+                    System.out.println(BelajarSingleton.getInstance().getVal());
+                }
+//                System.out.println(BelajarSingleton.getInstance().getVal()!=null?
+//                        BelajarSingleton.getInstance().getVal():"Tidak ada data");
+            }
+//            System.out.println("thread B");
+        });
+        Thread a = new Thread(()-> {
+//            System.out.println("Thread A");
+            System.out.println("Input some text :");
+            try(Scanner sc = new Scanner(System.in)){
+                BelajarSingleton.getInstance().setVal(sc.nextLine());
+            }
 
+//            try {
+//                sleep(1000);
+//                b.start();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        });
+        a.start();
+        b.start();
     }
 }
--- src/main/java/com/sinau/bareng/singleton/BelajarSingleton.java
+++ src/main/java/com/sinau/bareng/singleton/BelajarSingleton.java
@@ -1,2 +1,21 @@
-package com.sinau.bareng.singleton;public class BelajarSingleton {
+package com.sinau.bareng.singleton;
+
+public class BelajarSingleton {
+
+    private static BelajarSingleton _instance;
+    private String val;
+    public static synchronized BelajarSingleton getInstance(){
+        if (_instance==null){
+            _instance=new BelajarSingleton();
+        }
+        return _instance;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
 }
